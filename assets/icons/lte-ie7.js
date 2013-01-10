375d0b809629e80418ee3137e4c9e7f3ec64eb79
simurai <simulus@gmail.com>
2013-01-10 02:08:43 +0100

    Added List

--- assets/icons/lte-ie7.js
+++ assets/icons/lte-ie7.js
@@ -6,11 +6,29 @@ window.onload = function() {
 		el.innerHTML = '<span style="font-family: \'entypo\'">' + entity + '</span>' + html;
 	}
 	var icons = {
-			'icon-ellipsis' : '&#xe000;',
-			'icon-pictures' : '&#xe001;',
-			'icon-video' : '&#xe002;',
-			'icon-music' : '&#xe003;',
-			'icon-rocket' : '&#xe004;'
+			'icon-arrow-left' : '&#xe000;',
+			'icon-arrow-down' : '&#xe001;',
+			'icon-arrow-up' : '&#xe002;',
+			'icon-arrow-right' : '&#xe003;',
+			'icon-ellipsis' : '&#xe004;',
+			'icon-pictures' : '&#xe005;',
+			'icon-video' : '&#xe006;',
+			'icon-music' : '&#xe007;',
+			'icon-rocket' : '&#xe008;',
+			'icon-camera' : '&#xe009;',
+			'icon-checkmark' : '&#xe00a;',
+			'icon-cross' : '&#xe00b;',
+			'icon-minus' : '&#xe00c;',
+			'icon-plus' : '&#xe00d;',
+			'icon-cross-2' : '&#xe00e;',
+			'icon-minus-2' : '&#xe00f;',
+			'icon-plus-2' : '&#xe010;',
+			'icon-cross-3' : '&#xe011;',
+			'icon-user-add' : '&#xe012;',
+			'icon-user' : '&#xe013;',
+			'icon-erase' : '&#xe014;',
+			'icon-cog' : '&#xe015;',
+			'icon-export' : '&#xe016;'
 		},
 		els = document.getElementsByTagName('*'),
 		i, attr, html, c, el;
