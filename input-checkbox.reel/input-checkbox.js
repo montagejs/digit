864b61e86f295c54067f4921e9f7532489bd7681
Jacob Seidelin <jseidelin@nihilogic.dk>
2013-02-10 17:28:59 +0100

    Implement basic input-checkbox functionality

--- input-checkbox.reel/input-checkbox.js
+++ input-checkbox.reel/input-checkbox.js
@@ -1,16 +1,83 @@
 var Montage = require("montage").Montage,
     Component = require("montage/ui/component").Component,
-    NativeInputCheckbox = require("montage/ui/native/input-checkbox.reel").InputCheckbox;
+    PressComposer = require("montage/ui/composer/press-composer").PressComposer;
 
-exports.InputCheckbox = Montage.create(NativeInputCheckbox, {
+exports.InputCheckbox = Montage.create(Component, {
+    
+    _checked: {
+        value: false
+    },
+    
+    _disabled: {
+        value: false
+    },
 
-    hasTemplate: {value: true},
-
-    willPrepareForDraw: {
+    /**
+    The checked status of the InputCheckbox
+    @type {Boolean}
+    @default false
+    */
+    checked: {
+        get: function() {
+            return this._checked;
+        },
+        set: function(value) {
+            value = !!value;
+            if (this._checked !== value) {
+                this._checked = value;
+                this.needsDraw = true;
+            }
+        }
+    },
+    
+    /**
+    The disabled status of the InputCheckbox
+    @type {Boolean}
+    @default false
+    */
+    disabled: {
+        get: function() {
+            return this._disabled;
+        },
+        set: function(value) {
+            value = !!value;
+            if (this._disabled !== value) {
+                this._disabled = value;
+                this.needsDraw = true;
+            }
+        }
+    },
+   
+    handlePress: {
+        value: function(event) {
+            if (this.disabled) {
+                return false;
+            }
+            this.checked = !this.checked;
+            this.needsDraw = true;
+        }
+    },
+   
+    prepareForDraw: {
         value: function() {
-            NativeInputCheckbox.willPrepareForDraw.call(this);
-            this.element.classList.add("montage-InputCheckbox");
+            this.checked = this.element.hasAttribute("checked");
+            this.disabled = this.element.hasAttribute("disabled");
+        }
+    },
+    
+    draw: {
+        value: function() {
+            if (this.checked) {
+                this.element.classList.add("montage-InputCheckbox--checked");
+            } else {
+                this.element.classList.remove("montage-InputCheckbox--checked");
+            }
+            if (this.disabled) {
+                this.element.classList.add("montage-InputCheckbox--disabled");
+            } else {
+                this.element.classList.remove("montage-InputCheckbox--disabled");
+            }
         }
     }
-
+    
 });
