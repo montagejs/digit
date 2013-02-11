738413ae243e216f66d657427b1f3a6a21078b38
Jacob Seidelin <jseidelin@nihilogic.dk>
2013-02-11 17:21:42 +0100

    Input-radio no longer uses input element

--- input-radio.reel/input-radio.js
+++ input-radio.reel/input-radio.js
@@ -1,15 +1,86 @@
 var Montage = require("montage").Montage,
     Component = require("montage/ui/component").Component,
-    NativeInputRadio = require("montage/ui/native/input-radio.reel").InputRadio;
+    PressComposer = require("montage/ui/composer/press-composer").PressComposer;
+    
+exports.InputRadio = Montage.create(Component, {
+   
+    _checked: {
+        value: false
+    },
+    
+    _disabled: {
+        value: false
+    },
+    
+    _name: {
+        value: null
+    },
 
-exports.InputRadio = Montage.create(NativeInputRadio, {
-
-    hasTemplate: {value: true},
-
-    willPrepareForDraw: {
+    /**
+    The checked status of the InputRadio
+    @type {Boolean}
+    @default false
+    */
+    checked: {
+        get: function() {
+            return this._checked;
+        },
+        set: function(value) {
+            value = !!value;
+            if (this._checked !== value) {
+                this._checked = value;
+                this.needsDraw = true;
+            }
+        }
+    },
+    
+    /**
+    The disabled status of the InputRadio
+    @type {Boolean}
+    @default false
+    */
+    disabled: {
+        get: function() {
+            return this._disabled;
+        },
+        set: function(value) {
+            value = !!value;
+            if (this._disabled !== value) {
+                this._disabled = value;
+                this.needsDraw = true;
+            }
+        }
+    },
+   
+    handlePress: {
+        value: function(event) {
+            if (this.disabled) {
+                return false;
+            }
+            this.checked = !this.checked;
+            this.needsDraw = true;
+        }
+    },
+   
+    prepareForDraw: {
         value: function() {
-            NativeInputRadio.willPrepareForDraw.call(this);
-            this.element.classList.add("montage-InputRadio");
+            this.checked = this.element.hasAttribute("checked");
+            this.disabled = this.element.hasAttribute("disabled");
+        }
+    },
+    
+    draw: {
+        value: function() {
+            if (this.checked) {
+                this.element.classList.add("montage-InputRadio--checked");
+            } else {
+                this.element.classList.remove("montage-InputRadio--checked");
+            }
+            if (this.disabled) {
+                this.element.classList.add("montage-InputRadio--disabled");
+            } else {
+                this.element.classList.remove("montage-InputRadio--disabled");
+            }
         }
     }
 
