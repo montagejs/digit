fa1225e82196f1d865e83dfa5a1f010fcf67d3ff
Jacob Seidelin <jseidelin@nihilogic.dk>
2013-02-20 23:44:05 +0100

    Add initial tests for input-radio functionality

--- test/input-radio/input-radio-spec.js
+++ test/input-radio/input-radio-spec.js
@@ -9,8 +9,76 @@ TestPageLoader.queueTest("input-radio-test", function(testPage) {
         });
 
         describe("InputRadio", function() {
-            it("can be created", function() {
-                expect(testPage.test.inputRadio).toBeDefined();
+            describe("default", function () {
+                it("can be created", function() {
+                    expect(testPage.test.inputRadio1).toBeDefined();
+                });
+                var inputRadio1, inputRadio2, defaultChecked;
+                beforeEach(function() {
+                    if (!inputRadio1) {
+                        inputRadio1 = testPage.test.inputRadio1;
+                        //keep default values
+                        defaultChecked = inputRadio1.checked;
+                    }
+                    if (!inputRadio2) {
+                        inputRadio2 = testPage.test.inputRadio2;
+                        //keep default values
+                        defaultChecked = inputRadio2.checked;
+                    }
+                    //restore default values
+                    inputRadio1.checked = defaultChecked;
+                    inputRadio2.checked = defaultChecked;
+                });
+                describe("property", function() {
+                    describe("checked", function() {
+                        it("should have correct default", function() {
+                            expect(defaultChecked).toEqual(false);
+                        });
+                        it("can be set", function() {
+                            inputRadio1.checked = true;
+                            expect(inputRadio1.checked).toEqual(true);
+                        });
+                        it("should uncheck same-named radio buttons when checked", function () {
+                            inputRadio1.checked = true;
+                            inputRadio2.checked = true;
+                            expect(inputRadio1.checked).toEqual(false);
+                        });
+                        it("should only uncheck same-named radio buttons when checked", function () {
+                            inputRadio1.checked = true;
+                            inputRadio2.checked = true;
+                            expect(inputRadio2.checked).toEqual(true);
+                        });
+                    });
+                });
+                describe("interaction", function () {
+                    describe("press", function () {
+                        it("should check radio button if not already checked", function () {
+                            inputRadio1.checked = false;
+                            inputRadio1.handlePress();
+                            expect(inputRadio1.checked).toEqual(true);
+                        });
+                        it("should not change checked state if already checked", function () {
+                            inputRadio1.checked = true;
+                            inputRadio1.handlePress();
+                            expect(inputRadio1.checked).toEqual(true);
+                        });
+                    });
+                });
+            });
+            describe("initialization attributes", function () {
+                var inputRadio, defaultChecked;
+                beforeEach(function() {
+                    if (!inputRadio) {
+                        inputRadio = testPage.test.inputRadioChecked;
+                        //keep default values
+                        defaultChecked = inputRadio.checked;
+                    }
+                    //restore default values
+                    inputRadio.checked = defaultChecked;
+                });
+                it("should have expected property values", function () {
+                    expect(inputRadio.checked).toEqual(true);
+                });
             });
         });
     });
