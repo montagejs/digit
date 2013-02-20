b1c22c154e67bee88d62f09a54cdced430cc0ffd
Jacob Seidelin <jseidelin@nihilogic.dk>
2013-02-20 23:27:44 +0100

    Add initial tests for input-checkbox behavior

--- test/input-checkbox/input-checkbox-spec.js
+++ test/input-checkbox/input-checkbox-spec.js
@@ -9,8 +9,60 @@ TestPageLoader.queueTest("input-checkbox-test", function(testPage) {
         });
 
         describe("InputCheckbox", function() {
-            it("can be created", function() {
-                expect(testPage.test.inputCheckbox).toBeDefined();
+            describe("default", function () {
+                it("can be created", function() {
+                    expect(testPage.test.inputCheckbox).toBeDefined();
+                });
+                var inputCheckbox, defaultChecked;
+                beforeEach(function() {
+                    if (!inputCheckbox) {
+                        inputCheckbox = testPage.test.inputCheckbox;
+                        //keep default values
+                        defaultChecked = inputCheckbox.checked;
+                    }
+                    //restore default values
+                    inputCheckbox.checked = defaultChecked;
+                });
+                describe("property", function() {
+                    describe("checked", function() {
+                        it("should have correct default", function() {
+                            expect(defaultChecked).toEqual(false);
+                        });
+                        it("can be set", function() {
+                            inputCheckbox.checked = true;
+                            expect(inputCheckbox.checked).toEqual(true);
+                        });
+                    });
+                });
+                describe("interaction", function () {
+                    describe("press", function () {
+                        it("should check checkbox if not already checked", function () {
+                            inputCheckbox.checked = false;
+                            inputCheckbox.handlePress();
+                            expect(inputCheckbox.checked).toEqual(true);
+                        });
+                        it("should uncheck checkbox if already checked", function () {
+                            inputCheckbox.checked = true;
+                            inputCheckbox.handlePress();
+                            expect(inputCheckbox.checked).toEqual(false);
+                        });
+                    });
+                });
+            });
+            describe("initialization attributes", function () {
+                var inputCheckbox, defaultChecked;
+                beforeEach(function() {
+                    if (!inputCheckbox) {
+                        inputCheckbox = testPage.test.inputCheckboxAttributeChecked;
+                        //keep default values
+                        defaultChecked = inputCheckbox.checked;
+                    }
+                    //restore default values
+                    inputCheckbox.checked = defaultChecked;
+                });
+                it("should have expected property values", function () {
+                    expect(inputCheckbox.checked).toEqual(true);
+                });
             });
         });
     });
