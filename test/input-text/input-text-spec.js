c8b35b08c509922fa395face819ed2eb9cc09b28
Jacob Seidelin <jseidelin@nihilogic.dk>
2013-03-07 21:42:06 +0100

    initial input-text tests

--- test/input-text/input-text-spec.js
+++ test/input-text/input-text-spec.js
@@ -9,8 +9,33 @@ TestPageLoader.queueTest("input-text-test", function(testPage) {
         });
 
         describe("InputText", function() {
-            it("can be created", function() {
-                expect(testPage.test.inputText).toBeDefined();
+            describe("default", function () {
+                it("can be created", function() {
+                    expect(testPage.test.inputText).toBeDefined();
+                });
+                var inputText, defaultValue;
+                beforeEach(function() {
+                    if (!inputText) {
+                        inputText = testPage.test.inputText;
+                        //keep default values
+                        defaultValue = inputText.value;
+                    }
+                    //restore default values
+                    inputText.value = defaultValue;
+                });
+                describe("property", function() {
+                    describe("value", function() {
+                        it("TODO should have correct default", function() {
+                            expect(defaultValue).toEqual("");
+                        });
+                        it("can be set", function() {
+                            inputText.value = "a string";
+                            expect(inputText.value).toEqual("a string");
+                        });
+                        describe("behavior", function() {
+                        });
+                    });
+                });
             });
         });
     });
