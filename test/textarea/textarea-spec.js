0eaee4fa0c2a420584000d534497f034b5dd2411
Jacob Seidelin <jseidelin@nihilogic.dk>
2013-03-11 14:31:50 +0100

    Add tests for required, maxLength for textarea

--- test/textarea/textarea-spec.js
+++ test/textarea/textarea-spec.js
@@ -13,15 +13,19 @@ TestPageLoader.queueTest("textarea-test", function(testPage) {
                 it("can be created", function() {
                     expect(testPage.test.textarea).toBeDefined();
                 });
-                var textarea, defaultValue;
+                var textarea, defaultValue, defaultRequired, defaultMaxLength;
                 beforeEach(function() {
                     if (!textarea) {
                         textarea = testPage.test.textarea;
                         //keep default values
                         defaultValue = textarea.value;
+                        defaultRequired = textarea.required;
+                        defaultMaxLength = textarea.maxLength;
                     }
                     //restore default values
                     textarea.value = defaultValue;
+                    textarea.required = defaultRequired;
+                    textarea.maxLength = defaultMaxLength;
                 });
                 describe("property", function() {
                     describe("value", function() {
@@ -31,6 +35,24 @@ TestPageLoader.queueTest("textarea-test", function(testPage) {
                         it("can be set", function() {
                             textarea.value = "a string";
                             expect(textarea.value).toEqual("a string");
+                        });
+                    });
+                    describe("required", function() {
+                        it("TODO should have correct default", function() {
+                            expect(defaultRequired).toEqual(false);
+                        });
+                        it("TODO can be set", function() {
+                            textarea.required = true;
+                            expect(textarea.required).toEqual(true);
+                        });
+                    });
+                    describe("maxLength", function() {
+                        it("TODO should have correct default", function() {
+                            expect(defaultMaxLength).toEqual(-1);
+                        });
+                        it("TODO can be set", function() {
+                            textarea.maxLength = 400;
+                            expect(textarea.maxLength).toEqual(400);
                         });
                     });
                 });
