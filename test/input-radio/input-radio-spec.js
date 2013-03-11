1313721062c086f9d82dd58ff379b0eb36789a3b
Jacob Seidelin <jseidelin@nihilogic.dk>
2013-03-11 13:24:45 +0100

    Add test for required attribute

--- test/input-radio/input-radio-spec.js
+++ test/input-radio/input-radio-spec.js
@@ -13,26 +13,28 @@ TestPageLoader.queueTest("input-radio-test", function(testPage) {
                 it("can be created", function() {
                     expect(testPage.test.inputRadio1).toBeDefined();
                 });
-                var inputRadio1, inputRadio2, defaultChecked;
+                var inputRadio1, inputRadio2, defaultChecked1, defaultChecked2, defaultRequired;
                 beforeEach(function() {
                     if (!inputRadio1) {
                         inputRadio1 = testPage.test.inputRadio1;
                         //keep default values
-                        defaultChecked = inputRadio1.checked;
+                        defaultChecked1 = inputRadio1.checked;
+                        defaultRequired = inputRadio1.required;
                     }
                     if (!inputRadio2) {
                         inputRadio2 = testPage.test.inputRadio2;
                         //keep default values
-                        defaultChecked = inputRadio2.checked;
+                        defaultChecked2 = inputRadio2.checked;
                     }
                     //restore default values
-                    inputRadio1.checked = defaultChecked;
-                    inputRadio2.checked = defaultChecked;
+                    inputRadio1.checked = defaultChecked1;
+                    inputRadio2.checked = defaultChecked2;
+                    inputRadio1.required = defaultRequired;
                 });
                 describe("property", function() {
                     describe("checked", function() {
                         it("should have correct default", function() {
-                            expect(defaultChecked).toEqual(false);
+                            expect(defaultChecked1).toEqual(false);
                         });
                         it("can be set", function() {
                             inputRadio1.checked = true;
@@ -47,6 +49,17 @@ TestPageLoader.queueTest("input-radio-test", function(testPage) {
                             inputRadio1.checked = true;
                             inputRadio2.checked = true;
                             expect(inputRadio2.checked).toEqual(true);
+                        });
+                    });
+                });
+                describe("property", function() {
+                    describe("required", function() {
+                        it("should have correct default", function() {
+                            expect(defaultRequired).toEqual(false);
+                        });
+                        it("can be set", function() {
+                            inputRadio1.required = true;
+                            expect(inputRadio1.required).toEqual(true);
                         });
                     });
                 });
@@ -66,18 +79,23 @@ TestPageLoader.queueTest("input-radio-test", function(testPage) {
                 });
             });
             describe("initialization attributes", function () {
-                var inputRadio, defaultChecked;
+                var inputRadio, defaultChecked, defaultRequired;
                 beforeEach(function() {
                     if (!inputRadio) {
                         inputRadio = testPage.test.inputRadioChecked;
                         //keep default values
                         defaultChecked = inputRadio.checked;
+                        defaultRequired = inputRadio.required;
                     }
                     //restore default values
                     inputRadio.checked = defaultChecked;
+                    inputRadio.required = defaultChecked;
                 });
-                it("should have expected property values", function () {
+                it("should have expected checked property values", function () {
                     expect(inputRadio.checked).toEqual(true);
+                });
+                it("should have expected required property values", function () {
+                    expect(inputRadio.required).toEqual(true);
                 });
             });
         });
