cee1b860ed6f5e62c000d928439fb7ffb72d4122
António Afonso <antonio.afonso@gmail.com>
2013-04-12 20:33:27 -0700

    Add highlight to ListItem and moved CSS to List

--- ui/list-item.reel/list-item.js
+++ ui/list-item.reel/list-item.js
@@ -12,5 +12,15 @@ var Montage = require("montage").Montage,
     @extends module:montage/ui/component.Component
 */
 exports.ListItem = Montage.create(Component, /** @lends module:"ui/list-item.reel".ListItem# */ {
+    highlight: {
+        value: false
+    },
 
+    didCreate: {
+        value: function() {
+            this.defineBinding("classList.has('digit-ListItem--highlight')", {
+                "<-": "highlight"
+            });
+        }
+    }
 });
