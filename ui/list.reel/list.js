935d09982494b9793021dec88f0d7325aab3f5c0
António Afonso <antonio.afonso@gmail.com>
2013-04-16 15:26:22 -0700

    Fix list scrolling on iOS
    Make sure the page doesn't bounce when the list scrolling is past the top or bottom of the content area.

--- ui/list.reel/list.js
+++ ui/list.reel/list.js
@@ -7,5 +7,29 @@ exports.List = Montage.create(List, {
         value: function() {
             this._repetition = this.templateObjects.repetition;
         }
+    },
+
+    enterDocument: {
+        value: function(firstTime) {
+            if (firstTime) {
+                this.element.addEventListener("touchstart", this, false);
+            }
+        }
+    },
+
+    // Based on https://github.com/joelambert/ScrollFix/blob/master/scrollfix.js
+    handleTouchstart: {
+        value: function() {
+            var element = this.element,
+                startTopScroll = element.scrollTop;
+
+            if (startTopScroll <= 0) {
+                element.scrollTop = 1;
+            }
+
+            if (startTopScroll + element.offsetHeight >= element.scrollHeight) {
+                element.scrollTop = element.scrollHeight - element.offsetHeight - 1;
+            }
+        }
     }
 });
