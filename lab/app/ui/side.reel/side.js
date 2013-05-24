a62c93ced7c961b769117f5ef6c84d14a407c0a3
simurai <simulus@gmail.com>
2013-05-25 01:51:54 +0900

    Fixed and reorganized overview and lab

--- lab/app/ui/side.reel/side.js
+++ lab/app/ui/side.reel/side.js
@@ -1,4 +1,4 @@
 var Montage = require("montage/core/core").Montage,
     Component = require("montage/ui/component").Component;
 
-exports.Master = Montage.create(Component, {});
+exports.Side = Montage.create(Component, {});
