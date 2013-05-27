998203c1b190fcc06f31e089bbf7eba6742fd4db
António Afonso <antonio.afonso@gmail.com>
2013-05-27 14:19:56 -0700

    Remove unfinished components and lab

--- test/all.js
+++ test/all.js
@@ -2,14 +2,10 @@ require("montage-testing").run(require,[
     // Please keep in alphabetical order
     "test/button/button-spec",
     "test/checkbox/checkbox-spec",
-    "test/header/header-spec",
     "test/slider/slider-spec",
-    "test/map/map-spec",
     "test/number-field/number-field-spec",
-    "test/progress/progress-spec",
     "test/radio-button/radio-button-spec",
     "test/select/select-spec",
     "test/text-field/text-field-spec",
-    "test/toggle/toggle-spec"
     "test/text-area/text-area-spec"
 ]);
--- ui/tab-bar.reel/tab-bar.js
+++
@@ -1,4 +0,0 @@
-var Montage = require("montage/core/core").Montage,
-    Component = require("montage/ui/component").Component;
-
-exports.TabBar = Montage.create(Component, {});
