1cdb0cd21337f57cea14b9092acabdca04f585f7
António Afonso <antonio.afonso@gmail.com>
2013-06-03 17:46:30 -0700

    Update component authoring to specialize()

--- test/radio-button/radio-button-test.js
+++ test/radio-button/radio-button-test.js
@@ -1,5 +1,4 @@
-var Montage = require("montage").Montage;
 var TestController = require("montage-testing/test-controller").TestController;
 
-exports.Test = Montage.create(TestController, {
+exports.Test = TestController.specialize({
 });
