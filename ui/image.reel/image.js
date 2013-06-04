84e4242ab7e621a3a511a716b87802eedd606392
António Afonso <antonio.afonso@gmail.com>
2013-06-03 19:28:38 -0700

    Add Image

---
+++ ui/image.reel/image.js
@@ -0,0 +1,13 @@
+/**
+    @module ui/image.reel
+    @requires montage/ui/component
+*/
+var AbstractImage = require("montage/ui/base/abstract-image").AbstractImage;
+
+/**
+    @class Image
+    @extends Component
+*/
+exports.Image = AbstractImage.specialize(/** @lends Image# */ {
+
+});
