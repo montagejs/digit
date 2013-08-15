d160f52b64fbfe8f890bc31059c44ba83bf8596b
Adam Solove <asolove@gmail.com>
2013-08-14 17:57:27 -0700

    Rename Toggle to ToggleSwitch for consistency with Montage base class.

--- ui/toggle-switch.reel/toggle-switch.js
+++ ui/toggle-switch.reel/toggle-switch.js
@@ -1,14 +1,14 @@
 var Montage = require("montage/core/core").Montage,
     Component = require("montage/ui/component").Component;
 
-exports.Toggle = Montage.create(Component, {
+exports.ToggleSwitch = Montage.create(Component, {
     
     enterDocument: {
         value: function(firstTime) {
             if (firstTime) {
                 // Just temporary to test, should probably work on whole element.
                 this._element.addEventListener("click", function(event) {
-                    this.classList.toggle("digit-Toggle--checked");
+                    this.classList.toggle("digit-ToggleSwitch--checked");
                 }, true);
             }
         }
