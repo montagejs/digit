c9ce8c735d2006918afbd52b4722e4c105c30ea8
Adam Solove <asolove@gmail.com>
2013-09-18 12:00:48 -0400

    Documentation for alert.

--- overview/README.md
+++ overview/README.md
@@ -21,3 +21,4 @@
 ## Layout components
 
 ![List](../ui/list.reel/screenshot.png)
+![Alert](../ui/alert.reel/screenshot.png)
--- overview/ui/main.reel/main.js
+++ overview/ui/main.reel/main.js
@@ -1,3 +1,15 @@
 var Component = require("montage/ui/component").Component;
 
-exports.Main = Component.specialize({});
+exports.Main = Component.specialize({
+
+    draw: {
+        value: function() {
+            // There can normallt only be one alert, absolutely-positioned,
+            // so we need to manually mangle the dom and style them to appear inline.
+            var alerts = ["alert", "alert-light", "alert-dark"];
+            alerts.forEach(function(alertName){
+                this[alertName].element.appendChild(this[alertName]._overlay.element);
+            }.bind(this));
+        }
+    }
+});
