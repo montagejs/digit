90ef276f9b904d8ffe06dc3752bdbcdbb2d1f611
Thibault Zanini <thibaultzanini@gmail.com>
2014-11-10 21:38:06 -0800

    Remove the latest requirement to the matte component.

--- ui/list.reel/list.js
+++ ui/list.reel/list.js
@@ -1,9 +1,19 @@
-var List = require("matte/ui/list.reel").List;
+var Component = require("montage/ui/component").Component,
+    observeProperty = require("montage/frb/observers").observeProperty;
 
-exports.List = List.specialize({
+var deprecationWarning = require("montage/core/deprecate").deprecationWarning;
+
+exports.List = Component.specialize({
     constructor: {
         value: function List() {
             this.super();
+
+            this.defineBinding("_repetition.content", {"<-": "content"});
+
+            // Only use a contentController if content is not defined
+            this.defineBinding("content.defined() ? null : _repetition.contentController", {
+                "<-": "contentController"
+            });
         }
     },
 
@@ -17,6 +27,92 @@ exports.List = List.specialize({
         value: function(firstTime) {
             if (firstTime) {
                 this.element.addEventListener("touchstart", this, false);
+            }
+        }
+    },
+
+    /**
+     Description TODO
+     @private
+     */
+    _repetition: {
+        value: null
+    },
+
+    _scroller: {
+        value: null
+    },
+
+    /**
+     Description TODO
+     @type {Property}
+     @default null
+     */
+    delegate: {
+        value: null
+    },
+
+    content: {
+        value: null
+    },
+
+    contentController: {
+        value: null
+    },
+
+    axis: {
+        value: null
+    },
+
+    /**
+     Description TODO
+     @private
+     */
+    isSelectionEnabled: {
+        value: null
+    },
+
+    /**
+     * Threshold at which the list will fire a "listEnd" event. This is the ratio of
+     */
+    listEndEventThreshold: {
+        value: 1
+    },
+
+    // Initialization
+
+    // TODO we should probably support the programmatic initialization of a list; forwarding the childComponents
+    // along to the repetition
+    // I want to say that if somebody knows enough to do that they know enough to append the child components' elements
+    // into the repetition, not the list
+
+    observeProperty: {
+        value: function (key, emit, source, parameters, beforeChange) {
+            if (key === "objectAtCurrentIteration" || key === "currentIteration") {
+                deprecationWarning(key,":iteration.object");
+                if (this._repetition) {
+                    return this._repetition.observeProperty(key, emit, source, parameters, beforeChange);
+                }
+            } else {
+                return observeProperty(this, key, emit, source, parameters, beforeChange);
+            }
+        }
+    },
+
+    _fireEndEvent: {
+        value: function() {
+            this.dispatchEventNamed("listEnd");
+        }
+    },
+
+    handlePropertyChange: {
+        value: function(changeValue, key, object) {
+            if (key === "scrollY" || key === "_maxTranslateY") {
+                if (this._scroller && object === this._scroller) {
+                    if (this._scroller.scrollY >= (this._scroller._maxTranslateY * this.listEndEventThreshold) && this._scroller._maxTranslateY > 0) {
+                        this._fireEndEvent();
+                    }
+                }
             }
         }
     },
@@ -36,4 +132,5 @@ exports.List = List.specialize({
             }
         }
     }
+
 });
